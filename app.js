1b1eeb9bbbf6ed3d6c83371ca53f0f30be8b64de
Bình Dương Lê Thanh <17520279@gm.uit.edu.vn>
2020-05-18 10:59:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+//Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+
+//Import Routes
+const placesRoute = require('./routes/places');
+const contactRoute = require('./routes/contacts');
+const blogsRoute = require('./routes/blogs')
+const usersRoute = require('./routes/users');
+
+//Not available yet 
+// const uploadsRoutes = require('./routes/uploads')
+// app.use('/imguploads', uploadsRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/uploads', express.static('uploads'));
+app.use('/places', placesRoute);
+app.use('/blogs', blogsRoute);
+app.use('/contacts', contactRoute);
+app.use('/users', usersRoute);
+
+
+//connect to database
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => console.log('MongoDB Connected!'))
+    .catch(err => {
+        console.log(err);
+    });
+
+//Routes
+app.get('/', function (req, res) {
+    res.send('We are at server')
+})
+
+module.exports = app;
